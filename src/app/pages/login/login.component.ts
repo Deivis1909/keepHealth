20b932bce12a258dcc346267282f10d8e1003db8
Deivis1909 <113397116+Deivis1909@users.noreply.github.com>
2024-03-16 14:46:45 -0300

    tela inicial de login feita

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { Router } from '@angular/router';
 
-import { JsonPipe, NgFor } from '@angular/common';
 
-import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Validators } from '@angular/forms';
+
+
+import { FormsModule } from '@angular/forms';
+
 import { FormArray } from '@angular/forms';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
@@ -14,36 +16,34 @@ import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NgFor, ReactiveFormsModule, JsonPipe,MatButtonModule, MatDividerModule, MatIconModule],
+  imports: [FormsModule,MatButtonModule,MatDividerModule,MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  user ={
+    email:'',
+    senha:''
 
-profileForm = this.formBuilder.group({
-    email: ['', Validators.email],
-    senha: ['',Validators.minLength(4)],
-
-
-  });
-
-
-
-  constructor(private formBuilder: FormBuilder) {}
-
-  updateProfile() {
-    this.profileForm.patchValue({
-      email: 'Nancy',
-
-    });
   }
 
 
 
-  onSubmit() {
-    // TODO: Use EventEmitter with form value
-    console.warn(this.profileForm.value);
+
+
+  constructor(private router:Router) {}
+
+  login() {
+    return null;
+
+   }
+
+
   }
-}
+
+
+
+
+
 
